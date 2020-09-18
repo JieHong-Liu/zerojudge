94362852d8fdef232ffb75d04ae0c198e7ff2f42
jiehong <jiehong0914@gmail.com>
2020-09-18 09:40:40 +0800

    add to 20

--- a006.c
+++ a006.c
@@ -8,19 +8,19 @@ int main()
   int x1, x2;
   scanf("%d %d %d", &a, &b, &c);
   D = b * b - 4 * a * c;
-  x1 = (-b + sqrt(b * b - 4 * a * c)) / 2 * a;
-  x2 = (-b - sqrt(b * b - 4 * a * c)) / 2 * a;
+  x1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a); // 記得把2a也括起來，以免他以為是除2*a
+  x2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
   if (D > 0)
   {
-    printf("Two different roots x1=%d , x2=%d", x1, x2);
+    printf("Two different roots x1=%d , x2=%d\n", x1, x2);
   }
   else if (D == 0)
   {
-    printf("Two same roots x=%d", x1);
+    printf("Two same roots x=%d\n", x1);
   }
   else if (D < 0)
   {
-    printf("No real root")
+    printf("No real root\n");
   }
   return 0;
 }
