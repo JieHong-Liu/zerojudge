3871c0e389c155d6391e2b5a2d36630564bdc294
JieHong-Liu <jiehong0914@gmail.com>
2020-09-27 21:20:39 +0800

    update to a054

--- .vscode/a054.c
+++ .vscode/a054.c
@@ -9,22 +9,34 @@ int main()
     int city[] = {10, 11, 12, 13, 14, 15, 16, 17, 34, 18, 19, 20, 21, 22, 35, 23, 24, 25, 26, 27, 28, 29, 32, 30, 31, 33};
 
     char id[10]; // 要留一格存'\0'
-    int city_number[26];
     while (scanf("%s", id) != EOF)
     {
+        int check = asc2int(id[8]); // 存檢查碼
+        // printf("check number is %d\n", check);
+        int city_number[26];
         for (int i = 0; i < 26; i++)
         {
             int ten = city[i] / 10;
             int one = city[i] % 10;
             city_number[i] = ten + one * 9;
-            printf("i here is %d , city[i] is %d, and city_number[i] is %d \n", i, city[i], city_number[i]);
         }
+
         int sum = 0;
+
         for (int i = 0; i <= 8; i++)
         {
             sum = sum + asc2int(id[i]) * (8 - i);
         }
-        printf("sum is %d", sum);
+        for (int i = 0; i < 26; i++)
+        {
+            int check_num = (sum + city_number[i]) % 10;
+
+            if ((check + check_num) % 10 == 0)
+            {
+                printf("%c", 65 + i);
+            }
+        }
+        printf("\n");
     }
 
     return 0;
