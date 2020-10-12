7159c196315fc70755cd4e9263ba1dc30e3a5949
JieHong-Liu <jiehong0914@gmail.com>
2020-10-12 23:56:45 +0800

    fix 410

--- .vscode/a410.c
+++ .vscode/a410.c
@@ -5,16 +5,33 @@ int main()
     float x, y;
     // ax+ by = c
     // dx+ ey = f
-    scanf("%d%d%d%d%d%d", &a, &b, &c, &d, &e, &f);
-    int delta = a * e - b * d;
-    float delta_x = c * e - b * f;
-    float delta_y = a * f - c * d;
-    x = delta_x / delta;
-    y = delta_y / delta;
-    if (delta == 0)
+    // 當∆≠0 時，方程組恰有一解(x,y)
+    // 當∆=∆x=∆y=0，方程組有無限多解。[兩直線重合]
+    // 當∆=0，而∆x、∆y有一不為 0 時，方程組無解。[兩直線平行]
+    while (scanf("%d%d%d%d%d%d", &a, &b, &c, &d, &e, &f) != EOF)
     {
-        printf("No answer\n");
+        int delta = a * e - b * d;
+        float delta_x = c * e - b * f;
+        float delta_y = a * f - c * d;
+
+        if (delta == 0 && delta_x == 0 && delta_y == 0)
+        {
+            printf("Too many\n");
+            break;
+        }
+        else if (delta == 0)
+        {
+            if (delta_x != 0 || delta_y != 0)
+            {
+                printf("No answer");
+                break;
+            }
+        }
+
+        x = delta_x / delta;
+        y = delta_y / delta;
+
+        printf("x=%.2f\ny=%.2f\n", x, y);
     }
-    printf("x=%.2f\ny=%.2f\n", x, y);
     return 0;
 }
