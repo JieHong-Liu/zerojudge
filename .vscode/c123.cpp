119c801e5e8962451d851d4b104575f940a7870f
JieHong-Liu <jiehong0914@gmail.com>
2021-04-17 21:47:17 +0800

    Rail can not block me

--- .vscode/c123.cpp
+++ .vscode/c123.cpp
@@ -4,76 +4,76 @@
 using namespace std;
 int main()
 {
-    int num,leave_sure=0; 
-    while(!leave_sure)
+    int num, leave_sure = 0;
+    while (!leave_sure)
     {
         cin >> num; // numbers of the train carriage.
-        if(num == 0)
+        if (num == 0)
         {
             break;
         }
-        stack<int>* middle = new stack<int>[num];
-        queue<int>* right = new queue<int>[num];
-        for (int i = 1 ; i <= num ; i++)
+        stack<int> *middle = new stack<int>[num];
+        queue<int> *right = new queue<int>[num];
+        for (int i = 1; i <= num; i++)
         {
-            right->push(i); 
+            right->push(i);
         }
         // cout << (right->front()) << endl;
         // right->pop();// pop is to remove the first element.
 
         int *left = new int[num];
         int leave = 0;
-        while(!leave)
-        {   
-            // initialize the queue and stack.
-            while(!right->empty())
-            {
-                right->pop();
-            }
-            for (int i = 1 ; i <= num ; i++)
-            {
-                right->push(i); 
-            }
-            while(!middle->empty())
-            {
-                middle->pop();
-            }
+        while (!leave)
+        {
+
             int yes = 1;
-            
-            for (int i = 0 ; i < num ; i++)
+            for (int i = 0; i < num; i++)
             {
                 cin >> left[i];
-                if(left[0] == 0)
+                if (left[i] == 0)
                 {
                     leave = 1;
                     break;
                 }
-                if(middle->empty() == false && left[i] == middle->top() )
+                if (middle->empty() == false && left[i] == middle->top())
                 {
                     middle->pop();
                 }
                 else // means that the value is not in the middle station.
                 {
-                    while(right->empty() == false && left[i] != right->front() )
+                    while (right->empty() == false && left[i] != right->front())
                     {
                         middle->push(right->front());
                         // put the front element of the right into the middle stack.
                         right->pop(); // then remove it.
                     }
-                    if(right->empty() == true) // can't found in the right and the middle.
+                    if (right->empty() == true) // can't found in the right and the middle.
                     {
                         yes = 0;
-                        break;
                     }
-                    if(left[i] == right->front()) // found the right carriage.
+                    else if (left[i] == right->front()) // found the right carriage.
                     {
                         right->pop();
                     }
                 }
             }
-            if(left[0] != 0)
+
+            if (leave != 1)
             {
-                if(yes == 1)
+                // initialize the queue and stack.
+                while (!right->empty())
+                {
+                    right->pop();
+                }
+                for (int i = 1; i <= num; i++)
+                {
+                    right->push(i);
+                }
+                while (!middle->empty())
+                {
+                    middle->pop();
+                }
+                if (yes == 1)
                 {
                     cout << "Yes" << endl;
                 }
@@ -88,10 +88,6 @@ int main()
             }
         }
     }
-    
-
-    
-    
 
     return 0;
 }
